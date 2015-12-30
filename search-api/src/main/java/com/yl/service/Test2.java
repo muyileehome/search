84debfc5857534b31edc3294adc761b368723e7f
codermuyilee <582268374@qq.com>
2015-12-30 14:29:33 +0800

    删除无用的文件

--- search-api/src/main/java/com/yl/service/Test2.java
+++ search-api/src/main/java/com/yl/service/Test2.java
@@ -10,17 +10,9 @@ import java.util.regex.Pattern;
 /**
  * Created by Administrator on 2015/12/14.
  */
-public class Test2 extends Test {
+public class Test2 {
 
     public static void main(String[] args) {
-        String time="昨天09:05";
-       Pattern pattern = Pattern.compile("^昨天(([0-9]|:)*?$)");
-       Matcher matcher =pattern.matcher(time);
-        while (matcher.find()){
-            String partTime=matcher.group(1);
-            String dateTime=DateUtil.DateToString(new Date(System.currentTimeMillis()), DateStyle.YYYY_MM_DD);
-            System.out.println((dateTime+" "+partTime+":00"));
-            break;
-        }
+
     }
 }
